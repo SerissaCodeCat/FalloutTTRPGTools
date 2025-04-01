93706a15c332fb81c4ff92a57054dfead57c353e
Serissa The Code Cat <blacksnowreturns@gmail.com>
2025-04-01 12:16:47 -0400

    initial layout of the menu functionality

--- rusty-fallout/src/main.rs
+++ rusty-fallout/src/main.rs
@@ -1,4 +1,5 @@
-use rand::{random_range, Rng};
+use rand::random_range;
+use std::{io, result, string};
 
 fn main() {
     let goals = vec![
@@ -19,6 +20,88 @@ fn main() {
     println!("the goals for this project are to:");
     println!("{:?}", goals);
 
-    let roll_test = random_range(1..20);
-    println!("randomly rolled number is {}", roll_test);
+    let mut roll_test = random_range(1..20);
+    let mut roll_test2 = random_range(1..20);
+    println!("randomly rolled D20 is {}", roll_test);
+    roll_test = random_range(1..20);
+    println!(
+        "2D20 = {roll_test} & {roll_test2}, resulting in {}",
+        roll_test2 + roll_test
+    );
+
+    loop {
+        print!(
+            "\n 
+        1) LOOT TABLE \n 
+        2) LOCATION GENERATOR \n 
+        3) WEAPON LOOT GENERATOR \n 
+        0) EXIT \n 
+        \n 
+        ENTER YOUR OPTION \n"
+        );
+        let menu_option = convert_input_to_int();
+        match menu_option {
+            1 => loot_table(),
+            2 => location_generator(),
+            3 => weapon_table_setup(),
+            0 => exit_function(),
+            _ => break,
+        }
+    }
+}
+fn loot_table() {
+    print!("LOOT TABLE GOES HERE");
+}
+fn location_generator() {
+    println!("LOCATION GENERATOR GOES HERE");
+}
+fn weapon_table_setup() {
+    let mut weapon_rarity = 10;
+    println!("do you want a modified weapon? Y/N");
+    let modded_weapon = convert_input_to_bool();
+    println!("do you want to specify a rarity? Y/N");
+    let set_rarity = convert_input_to_bool();
+    if set_rarity {
+        print!("enter the maximum rarity level:");
+        weapon_rarity = convert_input_to_int();
+    }
+    basic_weapon_table(modded_weapon, weapon_rarity);
+}
+fn basic_weapon_table(create_modded_weapon: bool, create_weapon_of_rarity_level: i32) {
+    println!("BASIC WEAPON TABLE GOES HERE");
+    println!(
+        "select weapon rarity range: {}",
+        create_weapon_of_rarity_level.to_string()
+    );
+    println!("modify weapon: {}", create_modded_weapon.to_string());
+}
+fn weapon_mod_table(incoming: String) {
+    println!("weapon mod goes here, after taking in the weapon type of {incoming}, and limmiting mods to availiable mods for said weapon");
+}
+fn convert_input_to_bool() -> bool {
+    loop {
+        let mut tmp = String::new();
+        io::stdin()
+            .read_line(&mut tmp)
+            .expect("failed to read line.");
+        if tmp.trim() == "Y" || tmp.trim() == "y" {
+            return true;
+        } else if tmp.trim() == "N" || tmp.trim() == "n" {
+            return false;
+        } else {
+            println!("please enter Y or N");
+        };
+    }
+}
+fn convert_input_to_int() -> i32 {
+    let mut tmp = String::new();
+    println!("please enter a number");
+    io::stdin()
+        .read_line(&mut tmp)
+        .expect("could not read input.");
+    return tmp.trim().parse().expect("please enter a number");
+}
+fn exit_function() {
+    println!("data saving goes here, and then exit");
+    std::process::exit(0);
 }
