08bc24e06576cd7167089b0dc97910b7b79c9bb6
Serissa The Code Cat <blacksnowreturns@gmail.com>
2025-04-03 16:21:52 -0400

    started to make the more in depth weapon generation code

--- rusty-fallout/src/weapon.rs
+++ rusty-fallout/src/weapon.rs
@@ -1,4 +1,72 @@
 use std::{io, string};
+
+pub enum WeaponType {
+    SmallGun,
+    BigGun,
+    Energy,
+    Explosive,
+    Melee,
+    Unarmed,
+    Thowing,
+}
+pub enum DamageType {
+    Physical,
+    Energy,
+    Radiation,
+    Poison,
+}
+pub enum Range {
+    Close,
+    Medium,
+    Long,
+    Extream,
+}
+//pub enum AmmoType {
+//    .38,
+//    .308,
+//    .45,
+//    .50,
+//    10mm,
+//}
+pub struct Properties {
+    Accurate: bool,
+    Blast: bool,
+    CloseQuarters: bool,
+    Concealed: bool,
+    Debilitating: bool,
+    Gattling: bool,
+    Inaccurate: bool,
+    Mine: bool,
+    Nightvision: bool,
+    Parry: bool,
+    Recon: bool,
+    Reliable: bool,
+    Suppressed: bool,
+    Thrown: bool,
+    TwoHanded: bool,
+    Unreliable: bool,
+}
+pub struct DamageEffects {
+    burst: bool,
+    breaking: bool,
+    persistant: bool,
+    peircing: i8,
+    radioactive: bool,
+    spread: bool,
+    stun: bool,
+    vicious: bool,
+}
+pub struct Weapon {
+    name: String,
+    rarity: i8,
+    value: i32,
+    weapon_type: WeaponType,
+    damage_rating: i8,
+    damage_type: DamageType,
+    fire_rate: i8,
+    properties: Properties,
+    weight: f32,
+}
 pub fn weapon_table_setup() {
     let mut weapon_rarity = 10;
     println!("do you want a modified weapon? Y/N");
