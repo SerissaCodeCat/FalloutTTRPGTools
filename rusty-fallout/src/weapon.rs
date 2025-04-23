f81d11b05dfe4866e5435ff297283de740428745
Serissa The Code Cat <blacksnowreturns@gmail.com>
2025-04-23 14:48:26 -0400

    added Energy weapons

--- rusty-fallout/src/weapon.rs
+++ rusty-fallout/src/weapon.rs
@@ -12,6 +12,7 @@ pub enum WeaponType {
 pub enum DamageType {
     Physical,
     Energy,
+    EnergyAndPhysical,
     Radiation,
     Poison,
 }
@@ -403,11 +404,110 @@ pub fn weapon_table_setup() {
         },
     ];
     let EnergyWeaponList = vec![
-        "Institute Laser",
-        "Laser Musket",
-        "Laser Gun",
-        "Plasma Gun",
-        "Gamma Gun",
+        Weapon{
+            name:String::from("Institute Laser"),
+            rarity:2, 
+            value:50,
+            weapon_type: WeaponType::Energy,
+            damage_rating:3,
+            damage_type:DamageType::Energy,
+            damage_effects:DamageEffects{ 
+                burst:true,
+                ..default_damage_effects
+            },
+            fire_rate:3,
+            properties:Properties{
+                close_quarters:true,
+                inaccurate:true,
+                ..default_properties
+            },
+            weight:4.0,
+            ammunition:AMMO_TYPE[11].to_string(),
+            range:Range::Close
+        },
+        Weapon{
+            name:String::from("Laser Musket"),
+            rarity:1, 
+            value:57,
+            weapon_type: WeaponType::Energy,
+            damage_rating:5,
+            damage_type:DamageType::Energy,
+            damage_effects:DamageEffects{ 
+                peircing:1,
+                ..default_damage_effects
+            },
+            fire_rate:0,
+            properties:Properties{
+               two_handed:true,
+               ..default_properties
+            },
+            weight:13.0,
+            ammunition:AMMO_TYPE[11].to_string(),
+            range:Range::Medium
+        },
+        Weapon{
+            name:String::from("Laser Gun"),
+            rarity:2, 
+            value:69,
+            weapon_type: WeaponType::Energy,
+            damage_rating:4,
+            damage_type:DamageType::Energy,
+            damage_effects:DamageEffects{
+                peircing:1,
+                ..default_damage_effects
+            },
+            fire_rate:3,
+            properties:Properties{
+                close_quarters:true,
+                ..default_properties
+            },
+            weight:4.0,
+            ammunition:AMMO_TYPE[11].to_string(),
+            range:Range::Close
+        },
+        Weapon{
+            name:String::from("Plasma Gun"),
+            rarity:3, 
+            value:123,
+            weapon_type: WeaponType::Energy,
+            damage_rating:3,
+            damage_type:DamageType::EnergyAndPhysical,
+            damage_effects:DamageEffects{ 
+                burst:true,
+                ..default_damage_effects
+            },
+            fire_rate:3,
+            properties:Properties{
+                close_quarters:true,
+                inaccurate:true,
+                ..default_properties
+            },
+            weight:4.0,
+            ammunition:AMMO_TYPE[13].to_string(),
+            range:Range::Close
+        },
+        Weapon{
+            name:String::from("Gamma Gun"),
+            rarity:5, 
+            value:156,
+            weapon_type: WeaponType::Energy,
+            damage_rating:3,
+            damage_type:DamageType::Radiation,
+            damage_effects:DamageEffects{ 
+                peircing:1,
+                stun:true,
+                ..default_damage_effects
+            },
+            fire_rate:3,
+            properties:Properties{
+                blast:true,
+                inaccurate:true,
+                ..default_properties
+            },
+            weight:3.0,
+            ammunition:AMMO_TYPE[12].to_string(),
+            range:Range::Close
+        },
     ];
     let BigGuns = vec![
         "Fat Man",
