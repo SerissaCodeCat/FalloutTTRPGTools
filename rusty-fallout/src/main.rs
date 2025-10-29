bd8be15ed582903fc175c4ca86ac7310665ac623
Serissa Smith <serissacodecat@proton.me>
2025-10-29 00:34:28 -0400

    got basic UI template made in ratatui. now to make it work for this project

--- rusty-fallout/src/main.rs
+++ rusty-fallout/src/main.rs
@@ -1,7 +1,17 @@
-use color_eyre::{eyre::Result, owo_colors::OwoColorize};
-use crossterm::event::{self, Event};
-use rand::random_range;
-use ratatui::{DefaultTerminal, Frame};
+use color_eyre::owo_colors::OwoColorize;
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    terminal,
+};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
 use std::io;
 
 pub mod energy_weapons_list;
@@ -10,29 +20,88 @@ pub mod melee_weapons_list;
 pub mod small_guns_list;
 pub mod utility;
 pub mod weapon;
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    let terminal = ratatui::init(); //allowing the app complete
-    let result = run(terminal);
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init(); //allowing the app complete
+    let app_result = App::default().run(&mut terminal);
     ratatui::restore(); //return control of the Terminal to standard.
-    return result;
+    return app_result;
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if matches!(event::read()?, Event::Key(_)) {
-            break return Ok(());
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+    counter: u8,
+}
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        return Ok(());
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            //for the purposes here, we will only act on key PRESS, not release, or other.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event);
+            }
+            //if it is a differnt type of event ( _ is an all match) do nothing.
+            _ => {}
+        };
+        return Ok(());
+    }
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Esc => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
         }
     }
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
 }
-fn render(frame: &mut Frame) {
-    frame.render_widget("FALLOUT TTRPG HELPER", frame.area());
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from("Fallout TTRPG Helper");
+        let instructions = Line::from(vec![
+            "Decrement".into(),
+            "<left>".blue().bold(),
+            "Increment".into(),
+            "<right>".green().bold(),
+            "quit".into(),
+            "<Esc>".red().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.left_aligned())
+            .title_bottom(instructions.left_aligned())
+            .border_set(border::THICK);
+        let counter_text = Text::from(vec![Line::from(vec![
+            "value".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
 
-//pub struct App {
-//    exit: bool,
-//}
 //impl App {
 //    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
 //        while !self.exit {
@@ -73,24 +142,4 @@ fn render(frame: &mut Frame) {
 //fn exit_function() {
 //    println!("data saving goes here, and then exit");
 //    std::process::exit(0);
-//}
-//fn render(frame: &mut Frame) {
-//    let outer_layout = Layout::default()
-//        .direction(Direction::Vertical)
-//        .margin(1)
-//        .constraints(vec![Constraint::Percentage(20), Constraint::Percentage(80)])
-//        .split(frame.area());
-//
-//    frame.render_widget(
-//        Paragraph::new("Main Options").block(
-//            Block::new().title("MAIN OPTIONS").bold().fg(Color::Blue).borders(Borders::ALL),
-//        ),
-//        outer_layout[0],
-//    );
-//    frame.render_widget(
-//        Paragraph::new("Working Area").block(
-//            Block::new().title("WORKING AREA").bold(), //.borders(Borders::ALL),
-//        ),
-//        outer_layout[1],
-//    );
 //}
